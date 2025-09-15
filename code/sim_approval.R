0d6ed185ba435aa45a1498b95cc5daa834dc6403
Soubhik Barari <soubhikbarari@gmail.com>
2025-09-14 21:21:48 -0400

    updated prob sim

--- code/sim_approval.R
+++ code/sim_approval.R
@@ -24,43 +24,45 @@ tibble(y, P_y) %>%
 ## Suppose we magically knew that Donald Trump had a population approval rating of 42%.  
 ## Equivalently, 0.42 of the population approves of Trump. 
 
-## Draw a random sample of 1000 respondents where X = number who support Trump. 
-X <- rbinom(TODO, size=TODO, prob=TODO)
+set.seed(4716) ## Fun fact: this ensures that simulations are replicable
+
+## Draw a random sample of 1000 respondents where Y = number who support Trump. 
+Y <- rbinom(TODO, size=TODO, prob=TODO)
 
 ## How do we infer the proportion of the population who supports Trump from this sample?
 prop_approve <- TODO
 
 ## 1. How can we simulate 100 repeated samples of 1000 respondents? 
-X_repeated_100 <- rbinom(TODO, size=TODO, prob=0.42)
+Y_repeated_100 <- rbinom(TODO, size=TODO, prob=0.42)
 
 ## Visualize the resulting distribution of % approve.
-tibble(approval=X_repeated_100) %>%
+tibble(approval=Y_repeated_100) %>%
   mutate(prop_approve=TODO) %>% ## Hint: Hint: same operation as on line 31!
   ggplot(aes(x=prop_approve)) +
-  geom_histogram() +
+  geom_histogram(bins=100) +
   scale_x_continuous(limits = c(0.3, 0.5)) +
   theme_classic()
 
 ## 2. What about 10,000 repeated samples? 
-X_repeated_10k <- rbinom(TODO, size=TODO, prob=0.42)
+Y_repeated_10k <- rbinom(TODO, size=TODO, prob=0.42)
 
 ## Visualize the resulting distribution of % approve. 
 ## How does it compare to before?
-tibble(approval=X_repeated_10k) %>%
-  mutate(approval=TODO) %>% ## Hint: same operation as on line 31!
-  ggplot(aes(x=approval)) +
-  geom_histogram() +
+tibble(approval=Y_repeated_10k) %>%
+  mutate(prop_approve=TODO) %>% ## Hint: same operation as on line 31!
+  ggplot(aes(x=prop_approve)) +
+  geom_histogram(bins=100) +
   scale_x_continuous(limits = c(0.3, 0.5)) +
   theme_classic()
 
 ## 3. What about 100 repeated samples of 10,000 respondents?
-X_10k_repeated_100 <- rbinom(TODO, size=TODO, prob=0.42)
+Y_10k_repeated_100 <- rbinom(TODO, size=TODO, prob=0.42)
 
 ## Visualize the resulting distribution of % approve. 
 ## How does it compare to before?
-tibble(approval=X_10k_repeated_100) %>%
-  mutate(approval=TODO) %>% ## Hint: same operation as on line 31!
-  ggplot(aes(x=approval)) +
-  geom_histogram() +
+tibble(approval=Y_10k_repeated_100) %>%
+  mutate(prop_approve=TODO) %>% ## Hint: same operation as on line 31!
+  ggplot(aes(x=prop_approve)) +
+  geom_histogram(bins=100) +
   scale_x_continuous(limits = c(0.3, 0.5)) +
   theme_classic()
